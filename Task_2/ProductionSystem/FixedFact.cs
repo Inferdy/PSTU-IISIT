6840121ac14e2598c2df32c4bdc3ae3e185e911d
Inferdy <lime.clon@mail.ru>
2021-12-15 16:13:11 +0500

    Defined basic class: FixedFact

---
+++ Task_2/ProductionSystem/FixedFact.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductionSystem
+{
+    internal class FixedFact
+    {
+        public readonly string Name;
+        public readonly string Value;
+
+        public FixedFact(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
