359913f261dd2bfd387024e3cef0a59476fc9fc4
Inferdy <lime.clon@mail.ru>
2021-12-15 16:15:37 +0500

    Defined Rule types

---
+++ Task_2/ProductionSystem/Rule.cs
@@ -0,0 +1,42 @@
+﻿namespace ProductionSystem
+{
+    internal enum RuleState
+    {
+        Valid,
+        Invalid,
+        Active
+    }
+
+    internal class Rule
+    {
+        public readonly string Name;
+        public readonly string DirectValue;
+
+        protected IRulePart _rootRulePart;
+
+        public Rule(IRulePart rootRulePart, string name, string directValue)
+        {
+            _rootRulePart = rootRulePart;
+            Name = name;
+            DirectValue = directValue;
+        }
+
+        public virtual RuleState GetRuleState(IFactsProvider factsProvider)
+        {
+            switch (_rootRulePart.GetState(factsProvider))
+            {
+                case null:
+                    return RuleState.Valid;
+                case true:
+                    return RuleState.Active;
+                case false:
+                    return RuleState.Invalid;
+            }
+        }
+
+        public IEnumerable<FixedFact> Explain(IFactsProvider factsProvider)
+        {
+            return _rootRulePart.Explain(factsProvider);
+        }
+    }
+}
---
+++ Task_2/ProductionSystem/TwoHeadedRule.cs
@@ -0,0 +1,25 @@
+﻿namespace ProductionSystem
+{
+    internal class TwoHeadedRule : Rule
+    {
+        public readonly string ReverseValue;
+
+        public TwoHeadedRule(IRulePart rootRulePart, string name, string directValue, string reverseValue) : base(rootRulePart, name, directValue)
+        {
+            ReverseValue = reverseValue;
+        }
+
+        public override RuleState GetRuleState(IFactsProvider factsProvider)
+        {
+            switch (_rootRulePart.GetState(factsProvider))
+            {
+                case null:
+                    return RuleState.Valid;
+                case true:
+                    return RuleState.Active;
+                case false:
+                    return RuleState.Invalid;
+            }
+        }
+    }
+}
