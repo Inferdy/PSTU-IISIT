3ced863947593ac2077b78098bfd85570ddb4ef3
Sergey Khudanin <Seregahudanin@yandex.ru>
2021-12-22 23:48:53 +0500

    Implemented classes

--- Task_2/ProductionSystem/ProductionSystem/FactRulePart.cs
+++ Task_2/ProductionSystem/ProductionSystem/FactRulePart.cs
@@ -1,13 +1,34 @@
 namespace ProductionSystem
 {
-	public class FactRulePart : IRulePart
-	{
-		private string name;
-		private string value;
+    public class FactRulePart : IRulePart
+    {
+        private string name;
+        private string value;
 
-		public FactRulePart(string factName, string factValue)
-		{
-			throw new System.NotImplementedException();
-		}
-	}
+        public FactRulePart(string factName, string factValue)
+        {
+            name = factName;
+            value = factValue;
+        }
+
+        public Tuple<ExclusiveList<FixedFact>?, bool?> Explain(IFactsProvider factsProvider)
+        {
+            FixedFact? fact = factsProvider.GetFact(name);
+
+            if (fact == null)
+                return new Tuple<ExclusiveList<FixedFact>?, bool?>(null, null);
+
+            return new Tuple<ExclusiveList<FixedFact>?, bool?>(null, fact.Value == value);
+        }
+
+        public bool? GetValue(IFactsProvider factsProvider)
+        {
+            FixedFact? fact = factsProvider.GetFact(name);
+
+            if (fact == null)
+                return null;
+
+            return fact.Value == value;
+        }
+    }
 }
