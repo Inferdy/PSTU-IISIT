4c5190ba2628d3db644b27d36d3277e7de7c01f9
Sergey Khudanin <Seregahudanin@yandex.ru>
2021-12-22 23:50:55 +0500

    Implemented classes

--- Task_2/ProductionSystem/ProductionSystem/TwoHeadedRule.cs
+++ Task_2/ProductionSystem/ProductionSystem/TwoHeadedRule.cs
@@ -4,9 +4,28 @@ namespace ProductionSystem
 	{
 		private string reverseValue;
 
-		public TwoHeadedRule(string ruleName, string factName, int importance, string directValue, string reverseValue, IRulePart rootRulePart)
+		public TwoHeadedRule(
+			string ruleName,
+			string factName,
+			int importance,
+			string directValue,
+			string reverseValue,
+			IRulePart rootRulePart) : base(ruleName, factName, importance, directValue, rootRulePart)
 		{
-			throw new System.NotImplementedException();
+			this.reverseValue = reverseValue;			
 		}
-	}
+
+        public override FixedFact GetFact(bool boolValue)
+        {
+			if (boolValue)
+				return new FixedFact(FactName, directValue);
+			else
+				return new FixedFact(FactName, reverseValue);
+        }
+
+        public override bool IsActive(bool boolValue)
+        {
+			return true;
+        }
+    }
 }
