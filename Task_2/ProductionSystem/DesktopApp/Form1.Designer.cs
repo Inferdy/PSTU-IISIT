321d820b8fdf37218ae6fb84478a4615b578d8cd
Inferdy <lime.clon@mail.ru>
2021-12-24 18:01:37 +0500

    Finished WinForms Application

--- Task_2/ProductionSystem/DesktopApp/Form1.Designer.cs
+++ Task_2/ProductionSystem/DesktopApp/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,17 +23,225 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pathTextbox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.questionTextbox = new System.Windows.Forms.RichTextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.yesButton = new System.Windows.Forms.Button();
+            this.noButton = new System.Windows.Forms.Button();
+            this.idkButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.infoTextbox = new System.Windows.Forms.RichTextBox();
+            this.logTextbox = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.pathTextbox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.browseButton, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.startButton, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 29);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pathTextbox
+            // 
+            this.pathTextbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pathTextbox.Location = new System.Drawing.Point(3, 3);
+            this.pathTextbox.Name = "pathTextbox";
+            this.pathTextbox.Size = new System.Drawing.Size(632, 23);
+            this.pathTextbox.TabIndex = 0;
+            this.pathTextbox.Text = "C:\\Users\\kimvm\\Desktop\\KB.json";
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(641, 3);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(722, 3);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 29);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(800, 421);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.questionTextbox);
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(403, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 415);
+            this.panel1.TabIndex = 1;
+            // 
+            // questionTextbox
+            // 
+            this.questionTextbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.questionTextbox.Location = new System.Drawing.Point(0, 0);
+            this.questionTextbox.Name = "questionTextbox";
+            this.questionTextbox.ReadOnly = true;
+            this.questionTextbox.Size = new System.Drawing.Size(394, 386);
+            this.questionTextbox.TabIndex = 1;
+            this.questionTextbox.Text = "";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.yesButton);
+            this.flowLayoutPanel1.Controls.Add(this.noButton);
+            this.flowLayoutPanel1.Controls.Add(this.idkButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 386);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(394, 29);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // yesButton
+            // 
+            this.yesButton.Enabled = false;
+            this.yesButton.Location = new System.Drawing.Point(316, 3);
+            this.yesButton.Name = "yesButton";
+            this.yesButton.Size = new System.Drawing.Size(75, 23);
+            this.yesButton.TabIndex = 0;
+            this.yesButton.Text = "Не знаю";
+            this.yesButton.UseVisualStyleBackColor = true;
+            // 
+            // noButton
+            // 
+            this.noButton.Enabled = false;
+            this.noButton.Location = new System.Drawing.Point(235, 3);
+            this.noButton.Name = "noButton";
+            this.noButton.Size = new System.Drawing.Size(75, 23);
+            this.noButton.TabIndex = 1;
+            this.noButton.Text = "Нет";
+            this.noButton.UseVisualStyleBackColor = true;
+            // 
+            // idkButton
+            // 
+            this.idkButton.Enabled = false;
+            this.idkButton.Location = new System.Drawing.Point(154, 3);
+            this.idkButton.Name = "idkButton";
+            this.idkButton.Size = new System.Drawing.Size(75, 23);
+            this.idkButton.TabIndex = 2;
+            this.idkButton.Text = "Да";
+            this.idkButton.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.infoTextbox, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.logTextbox, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(394, 415);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // infoTextbox
+            // 
+            this.infoTextbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.infoTextbox.Location = new System.Drawing.Point(3, 3);
+            this.infoTextbox.Name = "infoTextbox";
+            this.infoTextbox.ReadOnly = true;
+            this.infoTextbox.Size = new System.Drawing.Size(388, 201);
+            this.infoTextbox.TabIndex = 1;
+            this.infoTextbox.Text = "";
+            // 
+            // logTextbox
+            // 
+            this.logTextbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logTextbox.Location = new System.Drawing.Point(3, 210);
+            this.logTextbox.Name = "logTextbox";
+            this.logTextbox.ReadOnly = true;
+            this.logTextbox.Size = new System.Drawing.Size(388, 202);
+            this.logTextbox.TabIndex = 2;
+            this.logTextbox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox pathTextbox;
+        private Button browseButton;
+        private Button startButton;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Panel panel1;
+        private RichTextBox questionTextbox;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button yesButton;
+        private Button noButton;
+        private Button idkButton;
+        private TableLayoutPanel tableLayoutPanel3;
+        private RichTextBox infoTextbox;
+        private RichTextBox logTextbox;
     }
 }
--- Task_2/ProductionSystem/DesktopApp/Form1.cs
+++ Task_2/ProductionSystem/DesktopApp/Form1.cs
@@ -1,10 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProductionSystem;
+
 namespace DesktopApp
 {
     public partial class Form1 : Form
     {
+        private Asker asker;
+        private Printer printer;
+        private Printer logger;
+        private object locker = new object();
+
         public Form1()
         {
             InitializeComponent();
+
+            asker = new Asker(yesButton, noButton, idkButton, questionTextbox);
+            printer = new Printer(infoTextbox);
+            logger = new Printer(logTextbox);
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    pathTextbox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private async void startButton_Click(object sender, EventArgs e)
+        {
+            IProductionSystem productionSystem = new ProductionSystem.ProductionSystem(pathTextbox.Text);
+
+            infoTextbox.Clear();
+            logTextbox.Clear();
+            questionTextbox.Clear();
+
+            if (Monitor.TryEnter(locker))
+                try
+                {
+                    await Task.Run(() =>
+                    {
+                        productionSystem.Run(printer, logger, asker);
+                    });
+
+                    MessageBox.Show("Ready");
+                }
+                finally
+                {
+                    Monitor.Exit(locker);
+                }
+        }
+
+        private class Asker : IAsker
+        {
+            private Button yesButton;
+            private Button noButton;
+            private Button idkButton;
+            private RichTextBox textBox;
+
+            private TaskCompletionSource<string>? tcs = null;
+
+            public Asker(Button yesButton, Button noButton, Button idkButton, RichTextBox textBox)
+            {
+                this.yesButton = yesButton;
+                this.noButton = noButton;
+                this.idkButton = idkButton;
+                this.textBox = textBox;
+
+                Wire();
+            }
+
+            private void Wire()
+            {
+                yesButton.Click += YesButton_Click;
+                noButton.Click += NoButton_Click;
+                idkButton.Click += IdkButton_Click;
+            }
+
+            private void IdkButton_Click(object? sender, EventArgs e)
+            {
+                tcs?.TrySetResult("да");
+            }
+
+            private void NoButton_Click(object? sender, EventArgs e)
+            {
+                tcs?.TrySetResult("нет");
+            }
+
+            private void YesButton_Click(object? sender, EventArgs e)
+            {
+                tcs?.TrySetResult("н/з");
+            }
+
+            public string Ask(string question)
+            {
+                tcs = new TaskCompletionSource<string>();
+
+                textBox.Invoke(() =>
+                {
+                    textBox.Text = question;
+                });
+
+                yesButton.Invoke(() =>
+                {
+                    yesButton.Enabled = true;
+                });
+
+                noButton.Invoke(() =>
+                {
+                    noButton.Enabled = true;
+                });
+
+                idkButton.Invoke(() =>
+                {
+                    idkButton.Enabled = true;
+                });
+
+                string result = tcs.Task.Result;
+
+                yesButton.Invoke(() =>
+                {
+                    yesButton.Enabled = false;
+                });
+
+                noButton.Invoke(() =>
+                {
+                    noButton.Enabled = false;
+                });
+
+                idkButton.Invoke(() =>
+                {
+                    idkButton.Enabled = false;
+                });
+
+                return result;
+            }
+        }
+
+        private class Printer : IPrinter
+        {
+            RichTextBox textBox;
+
+            public Printer(RichTextBox textBox)
+            {
+                this.textBox = textBox;
+            }
+
+            public void Print(string value)
+            {
+                textBox.Invoke(() =>
+                {
+                    textBox.AppendText(value);
+                });
+            }
+
+            public void PrintLine(string value)
+            {
+                textBox.Invoke(() =>
+                {
+                    textBox.AppendText(value);
+                    textBox.AppendText("\n");
+                });
+            }
+
+            public void PrintLine()
+            {
+                textBox.Invoke(() =>
+                {
+                    textBox.AppendText("\n");
+                });
+            }
         }
     }
 }
